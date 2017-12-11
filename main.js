270f0292c693a5737042c8d51425c59f52726ded
apple502j <33279053+apple502j@users.noreply.github.com>
2017-12-11 20:01:58 +0900

    Update With Bug!

--- main.js
+++ main.js
@@ -23,16 +23,29 @@ function isCommentOpen(user){
 }
 
 function isUser(user){
-	if (user.match(/[^a-zA-Z0-9-_]/)) return false;
-	if (user.length < 3) return false;
-	if (user.length > 20) return false;
+	if (user.match(/[^a-zA-Z0-9-_]/)) return false; // Only A-Z,a-z,0-9,-,_ is available
+	if (user.length < 3) return false; // Username must be 3 or more than.
+	if (user.length > 20) return false;// Username must be 20 or less than.
 	var _xhr=new XMLHttpRequest();
 	_xhr.open("GET","https://api.scratch.mit.edu/users/" + user,false);
 	_xhr.send();
-	var parsed=JSON.parse(_xhr.responceText);
-	if (parsed.code == "NotFound"){
+	if (_xhr.status == 404) {
 		return false;
-	} else{
+	} else {
 		return true;
 	}
+}
+
+function isScratcher(user){
+	var _xhr=new XMLHttpRequest();
+	_xhr.open("GET","http://scratch.mit.edu/users/" + user,false);
+	_xhr.send();
+	return _xhr.responceText.match(/<span class="group">\n +Scratcher/) != null;
+}
+
+function howLongByNow(user){
+	var _xhr=new XMLHttpRequest();
+	_xhr.open("GET","https://api.scratch.mit.edu/users/" + user,false);
+	_xhr.send();
+	var theday = _xhr.responceText.match(/\"joined\": \"20[0-9][0-9]-(0|1)[1-9]-[0-3][0-9]/);
 }
